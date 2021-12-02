d78924df0481ad4f92fa77921e1861ef8b41f39b
sltnsrh <lifenjoy8888@gmail.com>
2021-12-02 15:33:25 +0200

    TapeDeck class added

---
+++ src/Package1/TapeDeck.java
@@ -0,0 +1,28 @@
+package Package1;
+
+public class TapeDeck {
+    boolean canRecord = false;
+
+    void playTape() {
+        System.out.println("Type is plaing");
+    }
+
+    void recordTape() {
+        System.out.println("Its tape recording");
+    }
+}
+
+class TapeDeckTestDrive {
+    public static void main(String [] args) {
+        TapeDeck t = new TapeDeck();
+        t.canRecord = true;
+        t.playTape();
+
+        if(t.canRecord == true) {
+            t.recordTape();
+        }
+    }
+
+}
+
+
